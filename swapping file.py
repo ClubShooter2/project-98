7be70754783884debf2be211b54dece68ffa0c9c
ClubShooter2 <67182844+ClubShooter2@users.noreply.github.com>
2021-07-17 02:22:37 -0700

    Add files via upload

---
+++ swapping file.py
@@ -0,0 +1,9 @@
+def Swappfiledata():
+    file = input("Enter the file name")
+
+    numberofwords = 0
+    data_a = open(file1,"r")
+    data_b = open(file2,"r")
+    data_b = open(file1,"w")
+    data_a = open(file2,"w")
+Swappfiledata()
